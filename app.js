198a9846e87846fe528e33b3a074350718389db4
Abhigyan <abhigyansrivastav7@gmail.com>
2026-03-14 23:34:14 +0530

    tokenBucket implementation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const redis = new Redis();
 // 2. req comes check for the last 60 sec of window of that req time
 // 3. count the total no of req if lest then 60 then allow it
 
-    const rateLimiter = async (userId)=>{
+    const slidingWindowCounter = async (userId)=>{
         const window = 60;
         const limit = 60;
         const ttl = window * 2;
@@ -52,3 +52,41 @@ const redis = new Redis();
 // 
 // 
 //     
+
+
+// Token bucket
+// main idea is to allow bursts
+// have a bucket from which user req is validated 
+// refill at r per second rate
+// req = r - 1
+// -> Bucket holds max B tokens (burst capacity) -> 
+// Tokens refill at rate R per second -> 
+// Each request costs 1 token -> No tokens? Rejected.
+// 
+const tokenBucket = async(userId) =>{
+    const now = Date.now();
+    const rateKey = `rate:${userId}`
+    const maxTokens = 10;
+    const refillRate = 1;
+
+    const userBucket = await redis.hgetall(rateKey);
+    if(!userBucket.tokens){
+    await redis.hset(rateKey,{tokens: maxTokens -1 , lastRefill: now})
+    await redis.expire(rateKey,60)
+    return 'allowed';
+    }
+    const userTokens = Number(userBucket.tokens);
+    const userLastRefilledAt = Number(userBucket.lastRefill);
+    const timePassed = (now - userLastRefilledAt) / 1000; //sec
+    const refill = refillRate * timePassed;
+    const newTokens = Math.min(maxTokens, userTokens + refill)
+    if(newTokens<1){
+    await redis.hset(rateKey,{tokens: newTokens, lastRefill: now})
+    await redis.expire(rateKey,60)
+    return 'not allowed'
+    } 
+    await redis.hset(rateKey,{tokens: newTokens -1, lastRefill: now})
+    await redis.expire(rateKey,60)
+    return "allowed";
+
+}
